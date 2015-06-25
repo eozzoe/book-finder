230b5aae10b9385e4497f413a8ebdab90cf77525
Zoe Chan <zozoberry@gmail.com>
2015-06-25 14:07:43 +0000

    second time

--- application_controller.rb
+++ application_controller.rb
@@ -7,6 +7,44 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
   
+  get 'adventure' do
+    erb :adventure
+  end
   
+  get 'classics' do
+    erb :classics
+  end
+  
+  get 'dystopian' do
+    erb :dystopian
+  end
+  
+  get 'fantasy' do
+    erb :fantasy
+  end
+  
+  get 'historical' do
+    erb :historical
+  end
+  
+  get 'index' do
+    erb :index
+  end
+  
+  get 'memoir' do
+    erb :memoir
+  end
+  
+  get 'mystery' do
+    erb :mystery
+  end
+  
+  get 'realistic' do
+    erb :realistic
+  end
+  
+  get 'scifi' do
+    erb :scifi
+  end
 
 end
